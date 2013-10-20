8da5a229622cabed902c61efa67046c454223acb
Andrei A. Rusu <andreirusu@deepmind.com>
2013-10-20 13:36:58 +0100

    Updated logger to run in background

--- train-on-cifar/log.sh
+++ train-on-cifar/log.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -e 
 
-clear; cat log.txt
-
 while true ; do 
     date >> log.txt 
     for run in "$@"; do 
-        echo $run >> log.txt ; 
+        echo -n $run' ' >> log.txt  
         OMP_NUM_THREADS=1 torch trainer.lua -threads 1 -network $run/cifar.net -test | tail -n 3 | head -n 1 >> log.txt
     done
     echo "" >> log.txt
-    clear; cat log.txt
     sleep 300
 done
 
